c2793b9e0ac629d436398f34296d24247d9597e3
WerterBonfim <werter@hotmail.com.br>
2020-07-30 10:35:37 -0300

    login

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordRest from './LoginPasswordRest';
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="criar" element={<LoginCreate />} />
+                <Route path="perdeu" element={<LoginPasswordLost />} />
+                <Route path="resetar" element={<LoginPasswordRest />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/Components/Login/LoginPasswordRest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginPasswordRest = () => {
+    return (
+        <div>
+            LoginPasswordRest
+            
+        </div>
+    )
+}
+
+export default LoginPasswordRest;
